9ae65a7e23429e16ffb5e3d8cdfb5fdec5724fa1
Eric Montgomery <emontgomery@10-4.com>
2020-08-15 12:17:11 -0600

    Added ThirdPartyResource for unimported custom resources

---
+++ src/Kapitan.Core/ThirdPartyResource.cs
@@ -0,0 +1,14 @@
+﻿using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapitan.Core
+{
+    public class ThirdPartyResource : KubernetesObject
+    {
+        public V1ObjectMeta Metadata { get; set; }
+        public object Spec { get; set; }
+    }
+}
