3e56f44c343f0342c35fb4e582f5530b2ffb1525
Eric Montgomery <emontgomery@10-4.com>
2020-08-15 14:21:29 -0600

    Added first draft of simplified objects

---
+++ src/Kapitan.Core/Resources/Extensions/V1Beta1/Ingress.cs
@@ -0,0 +1,18 @@
+﻿using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapitan.Core.Resources.Extensions.V1Beta1
+{
+    public class Ingress : Extensionsv1beta1Ingress
+    {
+        public Ingress() : this(null)
+        {
+        }
+
+        public Ingress(V1ObjectMeta metadata = null, Extensionsv1beta1IngressSpec spec = null, Extensionsv1beta1IngressStatus status = null) : base("extensions/v1beta1", "Ingress", metadata, spec, status)
+        {
+        }
+    }
+}
---
+++ src/Kapitan.Core/Resources/V1/Service.cs
@@ -0,0 +1,18 @@
+﻿using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapitan.Core.Resources.V1
+{
+    public class Service : V1Service
+    {
+        public Service() : this(null)
+        {
+        }
+
+        public Service(V1ObjectMeta metadata = null, V1ServiceSpec spec = null, V1ServiceStatus status = null) : base("v1", "Service", metadata, spec, status)
+        {
+        }
+    }
+}
