c286dff720ac4157c1cb6e2f152c1dfa076d9c1a
Eric Montgomery <emontgomery@10-4.com>
2020-09-15 22:53:47 -0600

    Updated mock ApiVersion for Precompiled template

--- src/Kanyon.Core/PrecompiledManifestObject.cs
+++ src/Kanyon.Core/PrecompiledManifestObject.cs
@@ -6,7 +6,7 @@ namespace Kanyon.Core
 {
     public class PrecompiledManifestObject : IManifestObject
     {
-        public string ApiVersion => "github.com/ermontgo/kapitan/v1alpha1";
+        public string ApiVersion => "github.com/kanyon-k8s/kanyon/v1alpha1";
 
         public string Kind => nameof(PrecompiledManifestObject);
 
