bd86b735ec8af6f8d536d592d815adcae60b81a0
Eric Montgomery <ericm@remphoto.net>
2021-09-19 04:09:52 +0000

    Updated Csproj manifest loader to shell out to dotnet

--- src/Kanyon/Loaders/CsprojManifestLoader.cs
+++ src/Kanyon/Loaders/CsprojManifestLoader.cs
@@ -1,1 +1,33 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Kanyon.Core;
+
+namespace Kanyon.Loaders {
+    public class CsprojManifestLoader : Kanyon.Engine.Loaders.CompiledManifestLoader 
+    {
+        public new CsprojManifestLoader FromFile(FileInfo filePath) 
+        {
+            StartBuildProcess(filePath.FullName);
+
+            var manifestFile = new FileInfo(Path.Combine("kanyon_temp", "publish", "manifest.dll"));
+            base.FromFile(manifestFile);
+
+            return this;
+        }
+
+        private void StartBuildProcess(string projectFile)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"build \"{projectFile}\" --nologo -o \"kanyon_temp\\publish\" /p:AssemblyName=manifest"
+            };
+
+            var process = Process.Start(psi);
+            process.WaitForExit();
+        }
+    }
+}
